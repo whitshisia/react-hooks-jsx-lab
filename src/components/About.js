7e53d708bf7e5f98a6755e12f6a35d51812dacca
whitney shisia <shisiawhitney215@gmail.com>
2024-04-23 12:10:03 +0300

    react

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { image } from "../data/data";
+import { image} from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (<div id="about">About
+  <h2>About Me</h2>
+  <p>Hello world</p>
+  <img src={image} alt="I made this" /> 
+  </div>);
 }
 
 export default About;
